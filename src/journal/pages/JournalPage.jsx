43ec07427efa9a38ed6e8ab1971fe29140924f2d
mpatino <mspatino@gmail.com>
2023-09-06 14:58:36 -0300

    first commit

---
+++ src/journal/pages/JournalPage.jsx
@@ -0,0 +1,28 @@
+import { AddOutlined, MailOutline } from '@mui/icons-material'
+import { IconButton } from '@mui/material'
+import { JournalLayout } from '../layout/JournalLayout'
+import { NoteView, NothingSelectedView } from '../views'
+
+
+export const JournalPage = () => {
+  return (
+    <JournalLayout>
+      <NothingSelectedView />    
+        <IconButton
+        size='large'
+        sx={{ color: 'white',
+        backgroundColor: 'error.main',
+        ':hover': { backgroundColor: 'error.main', opacity: 0.9},
+        position: 'fixed',
+        right: 50,
+        bottom: 50
+        }}>
+          <AddOutlined sx={{ fontSize: 30}}>
+
+          </AddOutlined>
+          
+          </IconButton>  
+      {/* <NoteView /> */}
+    </JournalLayout>
+  )
+}
